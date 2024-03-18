640544df1a442374dadcc79888e3aff2d650bca0
ryota-09 <n.ryo950911@gmail.com>
2024-03-18 20:13:41 +0900

    フロント修正

--- src/components/BedRock.tsx
+++ src/components/BedRock.tsx
@@ -1,51 +1,62 @@
-import { BedrockRuntimeClient, InvokeModelCommand } from '@aws-sdk/client-bedrock-runtime';
-import { withSSRContext } from 'aws-amplify';
+"use client"
+import { BedrockRuntimeClient, InvokeModelWithResponseStreamCommand } from '@aws-sdk/client-bedrock-runtime';
 
 import { Amplify } from 'aws-amplify'
 import awsconfig from '../aws-exports';
+import { useEffect, useState } from 'react';
 
-Amplify.configure({
-  ...awsconfig,
-  ssr: true
-});
+export const BedRock = () => {
+  const [text, setText] = useState("")
+  useEffect(() => {
+    const fn = async () => {
+      const bedrock = new BedrockRuntimeClient({
+        region: 'us-east-1',
+        credentials: {
+          accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID ?? '',
+          secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY ?? '',
+        }
+      });
 
-export const BedRock = async () => {
-  const bedrock = new BedrockRuntimeClient({
-    region: 'us-east-1',
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? '',
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? '',
+      const prompt = `Human:${"大谷翔平について教えて"}\n\nAssistant:`;
+      const response = await bedrock.send(
+        new InvokeModelWithResponseStreamCommand({
+          modelId: 'anthropic.claude-v2:1',
+          contentType: 'application/json',
+          accept: '*/*',
+          body: JSON.stringify({
+            prompt,
+            // LLM costs are measured by Tokens, which are roughly equivalent
+            // to 1 word. This option allows you to set the maximum amount of
+            // tokens to return
+            max_tokens_to_sample: 2000,
+            // Temperature (1-0) is how 'creative' the LLM should be in its response
+            // 1: deterministic, prone to repeating
+            // 0: creative, prone to hallucinations
+            temperature: 1,
+            top_k: 250,
+            top_p: 0.99,
+            // This tells the model when to stop its response. LLMs
+            // generally have a chat-like string of Human and Assistant message
+            // This says stop when the Assistant (Claude) is done and expects
+            // the human to respond
+            stop_sequences: ['\n\nHuman:']
+          })
+        })
+      );
+      let completion = "";
+      if (response.body) {
+        const textDecoder = new TextDecoder("utf-8");
+
+        for await (const stream of response.body) {
+          const chunk = textDecoder.decode(stream.chunk?.bytes);
+          setText((prev) => prev + JSON.parse(chunk)["completion"])
+        }
+      }
+
     }
-  });
-
-  const prompt = `Human:${"大谷翔平について教えて"}\n\nAssistant:`;
-  const result = await bedrock.send(
-    new InvokeModelCommand({
-      modelId: 'anthropic.claude-v2:1',
-      contentType: 'application/json',
-      accept: 'application/json',
-      body: JSON.stringify({
-        prompt,
-        // LLM costs are measured by Tokens, which are roughly equivalent
-        // to 1 word. This option allows you to set the maximum amount of
-        // tokens to return
-        max_tokens_to_sample: 2000,
-        // Temperature (1-0) is how 'creative' the LLM should be in its response
-        // 1: deterministic, prone to repeating
-        // 0: creative, prone to hallucinations
-        temperature: 1,
-        top_k: 250,
-        top_p: 0.99,
-        // This tells the model when to stop its response. LLMs
-        // generally have a chat-like string of Human and Assistant message
-        // This says stop when the Assistant (Claude) is done and expects
-        // the human to respond
-        stop_sequences: ['\n\nHuman:']
-      })
-    })
-  );
-  console.log(JSON.parse(new TextDecoder().decode(result.body)));
+    fn()
+  }, [])
   return (
-    <></>
+    <>{text}</>
   )
 }
