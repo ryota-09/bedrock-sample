11da5588ce576b6e52a3c9f5bcd666afe7c62e55
ryota-09 <n.ryo950911@gmail.com>
2024-03-18 21:39:22 +0900

    メソッドを分割

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,22 @@
-import { BedRock } from "@/components/BedRock";
+"use client"
+
+import { postMessageWithStream } from "@/lib/bedrock"
+import { useState } from "react"
 
 export default function Home() {
+  const [prompt, setPrompt] = useState("")
+  const [botChat, setBotChat] = useState("")
+
+  const onSend = async (e: any) => {
+    e.preventDefault()
+    const prompt = e.target[0].value
+    setPrompt(prompt)
+    
+    await postMessageWithStream(prompt, (data) => {
+      setBotChat(prev => prev + data)
+    })
+  }
+
   return (
     <main>
       <div className="h-screen flex flex-col gap-4">
@@ -9,34 +25,38 @@ export default function Home() {
         </header>
         <main className="flex-1 flex flex-col p-4">
           <div className="grid gap-4">
-            <div className="flex flex-col items-end gap-1">
-              <div className="flex flex-col max-w-[75%] rounded-lg p-4 bg-gray-100">
-                <div className="flex items-center gap-2 text-sm">
-                  <div className="font-medium">You</div>
-                  <time className="opacity-70">2:26pm</time>
+            {prompt &&
+              <div className="flex flex-col items-end gap-1">
+                <div className="flex flex-col max-w-[75%] rounded-lg p-4 bg-gray-100">
+                  <div className="flex items-center gap-2 text-sm">
+                    <div className="font-medium">You</div>
+                    <time className="opacity-70">2:26pm</time>
+                  </div>
+                  <div className="mt-2">{prompt}</div>
                 </div>
-                <div className="mt-2">Hey, what's up?</div>
               </div>
-            </div>
-            <div className="flex flex-col items-start gap-1">
-              <div className="flex flex-col max-w-[75%] rounded-lg p-4 bg-gray-100">
-                <div className="flex items-center gap-2 text-sm">
-                  <div className="font-medium">ChatGPT</div>
-                  <time className="opacity-70">2:27pm</time>
+            }
+            {botChat &&
+              <div className="flex flex-col items-start gap-1">
+                <div className="flex flex-col max-w-[75%] rounded-lg p-4 bg-gray-100">
+                  <div className="flex items-center gap-2 text-sm">
+                    <div className="font-medium">ChatGPT</div>
+                    <time className="opacity-70">2:27pm</time>
+                  </div>
+                  <div className="mt-2">{botChat}</div>
                 </div>
-                <div className="mt-2">Hello! All good here. How can I assist you today?</div>
               </div>
-            </div>
+            }
           </div>
         </main>
         <div className="border-t p-4">
-          <form className="flex gap-4">
+          <form className="flex gap-4" onSubmit={onSend}>
             <input
               placeholder="Type a message"
               className="flex-1 p-3 rounded-lg border border-gray-300 focus:outline-none focus:ring focus:ring-gray-400"
               type="text"
             />
-            <button className="px-6 py-3 bg-gray-800 text-white rounded-lg hover:bg-gray-700 transition-colors">Send</button>
+            <button type="submit" className="px-6 py-3 bg-gray-800 text-white rounded-lg hover:bg-gray-700 transition-colors">Send</button>
           </form>
         </div>
       </div>
---
+++ src/lib/bedrock.ts
@@ -0,0 +1,47 @@
+import { BedrockRuntimeClient, InvokeModelWithResponseStreamCommand } from '@aws-sdk/client-bedrock-runtime';
+
+const bedrock = new BedrockRuntimeClient({
+  region: 'us-east-1',
+  credentials: {
+    accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID ?? '',
+    secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY ?? '',
+  }
+});
+
+export const postMessageWithStream = async (prompt: string, onMessage: (text: string) => void) => {
+  const _prompt = `Human:${`${prompt}`}\n\nAssistant:`;
+  const response = await bedrock.send(
+    new InvokeModelWithResponseStreamCommand({
+      modelId: 'anthropic.claude-v2:1',
+      contentType: 'application/json',
+      accept: '*/*',
+      body: JSON.stringify({
+        prompt: _prompt,
+        // LLM costs are measured by Tokens, which are roughly equivalent
+        // to 1 word. This option allows you to set the maximum amount of
+        // tokens to return
+        max_tokens_to_sample: 2000,
+        // Temperature (1-0) is how 'creative' the LLM should be in its response
+        // 1: deterministic, prone to repeating
+        // 0: creative, prone to hallucinations
+        temperature: 1,
+        top_k: 250,
+        top_p: 0.99,
+        // This tells the model when to stop its response. LLMs
+        // generally have a chat-like string of Human and Assistant message
+        // This says stop when the Assistant (Claude) is done and expects
+        // the human to respond
+        stop_sequences: ['\n\nHuman:']
+      })
+    })
+  );
+  if (response.body) {
+    const textDecoder = new TextDecoder("utf-8");
+
+    for await (const stream of response.body) {
+      const chunk = textDecoder.decode(stream.chunk?.bytes);
+      // setText((prev) => prev + JSON.parse(chunk)["completion"])
+      onMessage(JSON.parse(chunk)["completion"])
+    }
+  }
+}
